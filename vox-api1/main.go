42b4dd3791df92bb1993099c6dfa53b9fac22950
EmreCogac <emremta9@gmail.com>
2023-09-03 05:07:23 +0300

    first commit

---
+++ vox-api1/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"vox-api1/vox-api1/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	server *gin.Engine
+)
+
+func init() {
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatal("? Could not load environment variables", err)
+	}
+
+	initializers.ConnectDB(&config)
+
+	server = gin.Default()
+}
+
+func main() {
+
+	db := initializers.DB
+	var result string
+	db.Raw("SELECT name FROM users WHERE id = 1", 1).Scan(&result)
+
+	router := server.Group("/api")
+	router.GET("/healthchecker", func(ctx *gin.Context) {
+		message := "Welcome to Golang with Gorm and Postgres"
+		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": message})
+	})
+
+	router.GET("/sql", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"param": result,
+		})
+	})
+
+	log.Fatal(server.Run(":8080"))
+}
